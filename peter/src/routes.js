6d92a33747fc91795483746fa0081d4fef3a912d
petermking18 <petermking18@gmail.com>
2020-12-06 14:35:42 -0500

    added basic main page

--- peter/src/routes.js
+++ peter/src/routes.js
@@ -1,5 +1,7 @@
 import LandingPage from './Components/LandingPage';
+import MainPage from './Components/MainPage';
 
 export const ROUTES = [
+    { path: '/main', component: MainPage },
     { path: '/', component: LandingPage },
 ]
