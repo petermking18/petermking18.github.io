6092854e26b24435518edcdfa2caf2fdc19dca17
petermking18 <petermking18@gmail.com>
2020-12-09 17:28:37 -0500

    updated main page

--- peter/src/Components/MainPage.jsx
+++ peter/src/Components/MainPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './MainPage.scss';
 import Headshot from '../Headshot.jpg';
-import { Carousel } from './Carousel';
 import ContactForm from './ContactForm';
 
 export default class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
     render() {
         return <div id="mainpage">
             <div className="container py-5">
@@ -32,55 +38,51 @@ export default class MainPage extends React.Component {
                     <div id="collapseOne" className="collapse show" data-parent="#accordion">
                         <div className="card-body">
                             I'm a Computer Science student at Southern Methodist University in Dallas, TX.
-                            I am primarily focused on Software Engineering but have other interests as well...
+                            I'm primarily focused on Software Development and have experience in C++, React JS, HTML, CSS,
+                            Python, and more. Click the Projects tab below to see what I've been working on.
                         </div>
                     </div>
                 </div>
 
                 <div className="card">
                     <div className="card-header" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo">
-                        <h4 className="mb-0 ml-3 py-2 theme-gradient font-weight-bold">
-                            Projects
-                            {/* <button className="theme-gradient font-weight-bold btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseTwo">
-                                Projects
-                            </button> */}
-                        </h4>
+                        <h4 className="mb-0 ml-3 py-2 theme-gradient font-weight-bold">Projects</h4>
                     </div>
                     <div id="collapseTwo" className="collapse" data-parent="#accordion">
                         <div className="card-body">
-                            <div className="row hidden-md-up">
-                                <div className="col-md-6 mb-2">
-                                    <div className="card border border-secondary rounded">
-                                        <div className="card-block text-center px-2 py-1">
-                                            <h4 className="card-title mb-1">TravelGram</h4>
-                                            <h5 className="mb-1">
-                                                <span className="badge badge-pill theme-gradient-badge mx-1">React</span>
-                                                <span className="badge badge-pill theme-gradient-badge mx-1">HTML/CSS</span>
-                                                <span className="badge badge-pill theme-gradient-badge mx-1">Javascript</span>
-                                                <span className="badge badge-pill theme-gradient-badge mx-1">SQL</span>
-                                            </h5>
-                                            <p className="card-text p-y-1 mb-1">
-                                                A simple vacation sharing social website to document and discover new
-                                                trips around the globe.
+                            <div className="d-flex flex-wrap justify-content-around">
+                                <div className="pcardwrapper">
+                                    <a className="pcard text-center reg-a" href="https://github.com/petermking18/VacationGram" target="_blank">
+                                        <h4 className="mb-1">TravelGram</h4>
+                                        <h5 className="mb-1">
+                                            <span className="badge badge-pill theme-gradient-badge mx-1 my-1">React</span>
+                                            <span className="badge badge-pill theme-gradient-badge mx-1 my-1">HTML/CSS</span>
+                                            <span className="badge badge-pill theme-gradient-badge mx-1 my-1">Javascript</span>
+                                            <span className="badge badge-pill theme-gradient-badge mx-1 my-1">SQL</span>
+                                        </h5>
+                                        <p className="p-y-1 mb-1">
+                                            A simple vacation sharing social website to document and discover new
+                                            trips around the globe.
                                             </p>
-                                            <a className="btn btn-light" href="https://github.com/petermking18/VacationGram" target="_blank">See on Github</a>
-                                        </div>
-                                    </div>
+                                        <a href="https://github.com/petermking18/VacationGram" target="_blank">See on Github</a>
+                                    </a>
                                 </div>
-                                <div className="col-md-6">
-                                    <div className="card border border-secondary rounded">
-                                        <div className="card-block text-center px-2 py-1">
-                                            <h4 className="card-title mb-1">Algorithm Library</h4>
-                                            <h5 className="mb-1"><span className="badge badge-pill theme-gradient-badge">C++</span></h5>
-                                            <p className="card-text p-y-1 mb-1">
-                                                A collection of sorting and searching algorithms.
-                                                Sort data. Search graphs. Find travelling salesman solutions with naive, dynamic programming,
-                                                and metaheuristic techniques.
-                                            </p>
-                                            <a className="btn btn-light" href="https://github.com/petermking18/Algorithm-Library" target="_blank">See on GitHub</a>
-                                        </div>
-                                    </div>
+                                <div className="pcardwrapper">
+                                    <a className="pcard text-center reg-a" href="https://github.com/petermking18/Algorithm-Library" target="_blank">
+                                        <h4 className="mb-1">Algorithm Library</h4>
+                                        <h5 className="mb-1"><span className="badge badge-pill theme-gradient-badge">C++</span></h5>
+                                        <p className="p-y-1 mb-1">
+                                            A collection of sorting and searching algorithms, as well as a travelling salesman solver
+                                            with naive, dynamic programming, and metaheuristic techniques.
+                                        </p>
+                                        <a href="https://github.com/petermking18/Algorithm-Library" target="_blank">See on GitHub</a>
+                                    </a>
                                 </div>
+                                {/* <div className="pcardwrapper">
+                                    <div className="pcard text-center">
+                                        Hello
+                                    </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
@@ -92,10 +94,12 @@ export default class MainPage extends React.Component {
                     </div>
                     <div id="collapseThree" className="collapse" data-parent="#accordion">
                         <div className="card-body">
-                            <ContactForm/>
+                            <ContactForm />
                         </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     }
