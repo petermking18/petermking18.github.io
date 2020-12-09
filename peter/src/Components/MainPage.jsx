d2768165c84351febbc56d3e46da25c1ff505df6
petermking18 <petermking18@gmail.com>
2020-12-08 20:59:26 -0500

    redoing main page

--- peter/src/Components/MainPage.jsx
+++ peter/src/Components/MainPage.jsx
@@ -1,52 +1,50 @@
 import React from 'react';
 import './MainPage.scss';
+import Headshot from '../Headshot.jpg';
 
 export default class MainPage extends React.Component {
     render() {
         return <div id="mainpage">
-            <div className="container pt-2">
-                <h1 className="font-weight-bold">Peter King</h1>
+            <div className="container py-5">
+                <div className="row center-row">
+                    <img src={Headshot} alt="Headshot" id="Headshot" />
+                    <div className="col">
+                        <h1 className="theme-gradient font-weight-bold mb-0" id="Name">Peter King</h1>
+                        <br />
+                        <lead className="lead">Software Developer</lead>
+                    </div>
+                </div>
             </div>
-            <ul className="container mt-3">
-                <li>
-                    <h2 className="font-weight-bold"><a data-toggle="collapse" href="#aboutMe">About Me</a></h2>
-                    <div className="collapse" id="aboutMe">
-                        <div className="card card-body bg-dark">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Accumsan tortor posuere ac ut consequat semper viverra. Tellus id interdum velit laoreet. Semper risus in hendrerit gravida.
+            <div className="container" id="accordion">
+                <div className="card">
+                    <div className="card-header" id="headingOne" data-toggle="collapse" data-target="#collapseOne">
+                        <h5 className="mb-0">
+                            <button className="theme-gradient font-weight-bold font-size-larger btn btn-link" data-toggle="collapse" data-target="#collapseOne">
+                                About Me
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseOne" className="collapse show" data-parent="#accordion">
+                        <div className="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Aliquet eget sit amet tellus cras adipiscing enim eu. Amet nisl purus in mollis nunc sed id semper. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar.
                         </div>
                     </div>
-                </li>
-                <li>
-                    <h2 className="font-weight-bold"><a data-toggle="collapse" href="#myProjects">My Projects</a></h2>
-                    <div className="collapse" id="myProjects">
-                        <div className="container-fluid bg-dark">
-                            <div className="row flex-row flex-nowrap py-3" id="projectsContainer">
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card one</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card two</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card three</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card four</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card five</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card six</div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="card card-block bg-secondary pl-2 py-3">Card seven</div>
-                                </div>
-                            </div>
+                </div>
+                <div className="card">
+                    <div className="card-header" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo">
+                        <h5 className="mb-0">
+                            <button className="theme-gradient font-weight-bold btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
+                                Projects
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseTwo" className="collapse" data-parent="#accordion">
+                        <div className="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Aliquet eget sit amet tellus cras adipiscing enim eu. Amet nisl purus in mollis nunc sed id semper. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar.
                         </div>
                     </div>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     }
 }
